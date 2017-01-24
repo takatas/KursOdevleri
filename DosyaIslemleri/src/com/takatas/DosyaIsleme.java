d9e2e68b9b48fd8cc78dc317a4b4df7a227d3892
Mebi <Mebi@HP-PC>
2017-01-24 10:51:51 +0200

    neon commit denemesi 2

--- DosyaIslemleri/src/com/takatas/DosyaIsleme.java
+++ DosyaIslemleri/src/com/takatas/DosyaIsleme.java
@@ -45,6 +45,7 @@ public class DosyaIsleme {
 
 		System.out.println(dosyaOku());
 		System.out.println("-----------------------------------------");
+		System.out.println("-----------------------------------------");
 		
 
 	}
